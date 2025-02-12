02b051a5ce6f75b443340b39b89f939c263c0853
Reza Nikfal <mr.nikfal@gmail.com>
2025-02-12 01:14:51 -0600

    Add routes on server.js file

--- server.js
+++ server.js
@@ -6,9 +6,26 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.json({name: 'reza'})
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'})
 })
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `get bootcamp id: ${req.params.id}`})
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Create new bootcamp'})
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Update bootcamp id: ${req.params.id}`})
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Delete bootcamp id: ${req.params.id}`})
+})
+
 
 app.listen(PORT, () => {
     return console.log(`The server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
