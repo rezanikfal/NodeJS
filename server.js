b5c0e12eeae078820df2600758fae24c2d9894fc
Reza Nikfal <mr.nikfal@gmail.com>
2025-02-12 21:37:59 -0600

    Add Router middleware

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 
-dotenv.config({path: './config/config.env'})
+dotenv.config({path: './config/config.env'});
 const app = express();
 
-const PORT = process.env.PORT
-
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
-})
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `get bootcamp id: ${req.params.id}`})
-})
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success: true, msg: 'Create new bootcamp'})
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Update bootcamp id: ${req.params.id}`})
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Delete bootcamp id: ${req.params.id}`})
-})
+app.use('/api/v1/bootcamps', bootcamps);
 
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     return console.log(`The server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
